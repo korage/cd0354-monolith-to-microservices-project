048cf53c516cec775dc8d62358bfdf38807c149c
korage <gloriouschild11@gmail.com>
2022-07-20 01:38:14 +0100

    updated all configfiles and redeployed

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a770910d481ce456db885c47488948e2-180455883.us-west-1.elb.amazonaws.com:3000/api/v0'
 };
 
 /*
